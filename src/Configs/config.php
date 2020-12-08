91d00fabb5d3e7c6be8df6a69d4af308ea8eb294
smr seddighy <smr.seddighy@gmail.com>
2020-12-08 17:22:52 +0330

    add laravel support (#16)
    * Add laravel support

---
+++ src/Configs/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    // SMS.ir Api Key
+    'api-key' => env('SMSIR_API_KEY', 'Your api key'),
+    // SMS.ir Secret Key
+    'secret-key' => env('SMSIR_SECRET_KEY', 'Your secret key'),
+    // Your sms.ir line number
+    'line-number' => env('SMSIR_LINE_NUMBER', 'Your Sms.ir Line Number'),
+    //HTTP Request Timeout (seconds)
+    'request-timeout'=>10
+];
---
+++ src/Facades/SMSIR.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Amirbagh75\SMSIR\Facades;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class SMSIR extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'smsir';
+    }
+}
